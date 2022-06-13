c1b77c1473675827ed0efc6f6bd382f39e54b204
+ <+>
2022-06-13 12:43:17 +0300

    commit tree 54625f709dbe0c081f42ca870cb35cd45f41ac11

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function () {
+  newDom();
+});
+function newDom() {
+  document.getElementById("text").innerHTML = "This is really cool!";
+}
+console.log(updateDom)
